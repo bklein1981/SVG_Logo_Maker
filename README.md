8b8d5396d61d3d62b17a7931ac9c52c8265014cc
Brian Klein <bklein1981@gmail.com>
2023-08-11 12:20:37 -0500

    Readjusted tests to ensure they still pass based on changes to code. Added draft readme.md

---
+++ README.md
@@ -0,0 +1,36 @@
+# SVG Logo Maker
+
+  
+
+  ## Description
+  The SVG Logo Maker is a tool to automate the process of creating an simple SVG Logo. As a developer I want a way to generate a simple logo for my projects, to decrease the amount of time I need to spend on the task of creating a logo. This tool provides the functionality in a node application.
+  
+  ## Table of Contents
+  1. [Installation](#Installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contribute](#contribute)
+  5. [Tests](#tests)
+  6. [Questions](#questions)
+  
+  ## Installation
+  No installation necessary, just run node index.js from you CLI.
+  
+  ## Usage
+  To Run the SVG generator, use node index.js from the command line, answer the questions provided, and an SVG logo matching your criteria will be placed in the examples folder.
+  
+  ## License
+  This software is not covered under any license.
+  
+  
+  
+  ## How to Contribute
+  N/A
+  
+  ## Tests
+  N/A
+  
+  ## Questions
+  Github: https://github.com/bklein1981
+  
+  email: You may contact me at bklein1981@gmail.com if you have additional questions.
--- test/shape.test.js
+++ test/shape.test.js
@@ -6,22 +6,22 @@ describe('Color', () => {
     describe('Triangle', () => {
         it('should return a string with the correponding SVG file with a given color', () => {
             const shape = new Triangle();
-            shape.color = 'blue';
-            expect(shape.render()).toEqual('<polygon points="150, 5 250, 200 50, 200" fill="blue"/>')
+            color = 'blue';
+            expect(shape.render(color)).toEqual('<polygon points="150, 5 250, 200 50, 200" fill="blue"/>')
         });
     });
     describe('Circle', () => {
         it('should return a string with the correponding SVG file with a given color', () => {
             const shape = new Circle();
-            shape.color = 'red';
-            expect(shape.render()).toEqual('<circle cx="150" cy="100" r="100" fill="red" />');
+            color = 'red';
+            expect(shape.render(color)).toEqual('<circle cx="150" cy="100" r="100" fill="red" />');
         });
     });
     describe('Square', () => {
         it('should return a string with the correponding SVG file with a given color', () => {
             const shape = new Square();
-            shape.color = 'green';
-            expect(shape.render()).toEqual('<rect x="50" y="0" rx="0" ry="0" width="200" height="200" fill="green"/>');
+            color = 'green';
+            expect(shape.render(color)).toEqual('<rect x="50" y="0" rx="0" ry="0" width="200" height="200" fill="green"/>');
         });
     });
 });
